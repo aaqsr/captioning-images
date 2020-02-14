1c83f6e1a9e29ac6d32fb94db6c2e12f1a23cb64
Awab Qureshi <awab.qureshi@gmail.com>
2020-02-14 17:24:37 +0500

    More Callibration

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from PIL import Image, ImageFont, ImageDraw, ImageOps
 import os
 
 p = "/Users/apple/Documents/pyth image work/images"
+skipped = []
+
 if os.path.exists(p) and os.path.isdir(p):
     for name in os.listdir(p):
         if name != '.DS_Store':
             print(name)
             filename = name.replace('.jpg', '')
-            print(quotes.quotes[filename])
-            img = Image.open(
-                "/Users/apple/Documents/pyth image work/images/{}".format(name))
-            img = cropit(img)
-            img = add_border(img)
-            drawText(quotes.quotes[filename], img)
-            drawTitle(filename, img)
-            img.save("out/{}".format(name))
+            try:
+                print(quotes.quotes[filename])
+                img = Image.open(
+                    "/Users/apple/Documents/pyth image work/images/{}".format(name))
+                img = cropit(img)
+                img = add_border(img)
+                drawText(quotes.quotes[filename], img)
+                drawTitle(filename, img)
+                img.save("out/{}".format(name))
+            except:
+                print('Error, likely quote not found, skipping')
+                skipped.append(filename)
 
+print('Skipped the following:\n')
+print(skipped)
+                
 # TODO WHAT IF WE PUT THE TITLE ON THE TOP??
 # TODO Fix the title's placement, it's clipping through the quotes. Also fix the quote's placement
