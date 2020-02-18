e7efec8f1114d52f1fb5dc6a4dfd5e1b485c6d6e
Awab Qureshi <awab.qureshi@gmail.com>
2020-02-18 21:25:08 +0500

    Added documentation to file

--- quotes.py
+++ quotes.py
@@ -1,3 +1,6 @@
 quotes = {
-    'test':'This is a sample quote on a sample image. This photo is by Sharon McCutcheon on Unsplash!'
+    # The format for adding your own quotes goes something like this:
+    # 'filename':'<quote>',
+    # Be careful to not forget the comma at the end or to add any ' other than the ones required. If you want to use ' use " instead.
+    'test':'This is a sample quote on a sample image. This photo is by Sharon McCutcheon on Unsplash!',
 }
